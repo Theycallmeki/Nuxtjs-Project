90bed27e56ebf10cbea508eea690ed9a46138f11
Theycallmeki <laxamana.kian13@gmail.com>
2025-05-02 00:15:25 +0800

    negg

--- components/header.vue
+++ components/header.vue
@@ -2,8 +2,13 @@
   <header>
     <h1>Welcome to My Nuxt.js App</h1>
     <nav>
-      <nuxt-link to="/">Home</nuxt-link> |
-      <nuxt-link to="/about">About</nuxt-link>
+      <div class="dropdown">
+        <button class="dropdown-button">Menu</button>
+        <div class="dropdown-content">
+          <nuxt-link to="/">Home</nuxt-link>
+          <nuxt-link to="/about">About</nuxt-link>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
@@ -23,5 +28,50 @@ header {
 
 nav {
   margin-top: 10px;
+}
+
+/* Style for dropdown button */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 10px 15px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown-button:hover {
+  background-color: #3e8e41;
+}
+
+/* Dropdown menu (hidden by default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+/* Show the dropdown menu when hovering over the button */
+.dropdown:hover .dropdown-content {
+  display: block;
 }
 </style>
